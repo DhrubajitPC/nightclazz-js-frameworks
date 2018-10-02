932d4b81f8cbb14f8e51159cedb99295b9d585c9
dhrubajitpc <dhrubajit1992@gmail.com>
2018-10-02 15:34:02 +0800

    props

--- ng-likeable/src/app/like-button/like-button.component.ts
+++ ng-likeable/src/app/like-button/like-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-like-button',
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class LikeButtonComponent implements OnInit {
 
   public isLiked = true;
+  @Input() public nbLike: number;
 
   constructor() { }
 
