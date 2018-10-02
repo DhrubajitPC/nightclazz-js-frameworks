414c893cc6dd1b86dd139021d5a186f8f3f037e1
dhrubajitpc <dhrubajit1992@gmail.com>
2018-10-02 15:50:36 +0800

    view

--- react-likeable/src/components/like-button.js
+++ react-likeable/src/components/like-button.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 export default class LikeButton extends React.Component {
   render(){
-    return <div></div>
+    return (
+      <div>
+        <h1>Liking a button in react</h1>
+        <button className="icon">❤</button>
+      </div>
+    )
   }
 }
