3290ccbb803d6ed41e75b567213707d2bb048336
dhrubajitpc <dhrubajit1992@gmail.com>
2018-10-02 15:56:28 +0800

    state

--- react-likeable/src/components/like-button.js
+++ react-likeable/src/components/like-button.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import './like-button.css';
 
 export default class LikeButton extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLiked: true,
+    };
+  }
+
   render(){
+    const iconClass = this.state.isLiked ? 'icon liked' : 'icon';
     return (
       <div>
         <h1>Liking a button in react</h1>
-        <button className="icon">❤</button>
+        <button className={iconClass}>❤</button>
       </div>
     )
   }
